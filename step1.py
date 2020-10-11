e8363a54174d79287e22d5039ea30c52f624990c
mmakoto623 <46061108+mmakoto623@users.noreply.github.com>
2020-10-11 22:52:59 +0900

    Delete step1.py

--- step1.py
+++
@@ -1,6 +0,0 @@
-#STEP: 1 盤面の情報取得
-H,W,N = [int(i) for i in input().split()]
-S = list([input() for i in range(H)])
-for i in range(N):
-    y,x = [int(i) for i in input().split()]
-    print(S[y][x])
